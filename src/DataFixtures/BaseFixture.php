1bf87802877cbaa11717a753e685118ff17c3069
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-06-22 16:23:02 +0200

    Create BaseFixture.php

---
+++ src/DataFixtures/BaseFixture.php
@@ -0,0 +1,43 @@
+<?php
+namespace TurboLabIt\TLIBaseBundle\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+
+
+abstract class BaseFixture extends Fixture
+{
+    /** @var ObjectManager */
+    protected $manager;
+
+    /** @var Generator */
+    protected $faker;
+
+    abstract protected function loadData(ObjectManager $manager);
+
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+        $this->faker = Factory::create();
+
+        $this->loadData($manager);
+    }
+
+
+    protected function createMany(string $className, int $count, callable $factory)
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $entity = new $className();
+            $factory($entity, $i);
+
+            $this->manager->persist($entity);
+            // store for usage later as App\Entity\ClassName_#COUNT#
+            $this->addReference($className . '_' . $i, $entity);
+        }
+
+        $this->manager->flush();
+    }
+}
