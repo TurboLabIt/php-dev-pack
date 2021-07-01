b7990754c0f087522208c4a9afeebb40fc9772ea
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-07-01 09:21:41 +0200

    Update TrimmerExtension.php

--- src/Twig/TrimmerExtension.php
+++ src/Twig/TrimmerExtension.php
@@ -4,18 +4,13 @@ namespace TurboLabIt\TLIBaseBundle\Twig;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
+
 class TrimmerExtension extends AbstractExtension
 {
     public function getFilters()
     {
         return [
-            new TwigFilter('trimmer', [$this, 'trim']),
+            new TwigFilter('trimmer', [TrimmerExtensionRuntime::class, 'trim']),
         ];
-    }
-
-
-    public function trim(string $value): string
-    {
-        return trim($value);
     }
 }
