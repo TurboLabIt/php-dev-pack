e7d6d69a6d3969320ee9a9695cd5af189179f64c
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-07-06 20:10:37 +0200

    Update TrimmerRuntime.php

--- src/Twig/TrimmerRuntime.php
+++ src/Twig/TrimmerRuntime.php
@@ -4,10 +4,23 @@ namespace TurboLabIt\TLIBaseBundle\Twig;
 use Twig\Extension\RuntimeExtensionInterface;
 
 
-class TrimmerRuntime implements RuntimeExtensionInterface
+class TliTwigRuntime implements RuntimeExtensionInterface
 {
-    public function trim(string $value): string
+    public function trim(string $input): string
     {
-        return trim($value);
+        return trim($input);
+    }
+    
+    
+    public function friendlyNum(string|float|int $input): string
+    {
+        $locale = Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']) ?? 'it_IT';
+        return (new \NumberFormatter($locale, \NumberFormatter::DECIMAL))->format($input);
+    }
+    
+    
+    public function friendlyDate(\DateTime $date): string
+    {
+        return $date->format('Y-m-d');
     }
 }
