32a896c2e0b6971264747daf61e6f0ccf6c7d471
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-09-04 15:29:59 +0200

    Update UndefinedMagicMethodException.php

--- src/Exception/UndefinedMagicMethodException.php
+++ src/Exception/UndefinedMagicMethodException.php
@@ -6,7 +6,7 @@ class UndefinedMagicMethodException extends \Exception
 {
     public function __construct($object, string $calledMethod, $code = 0, Throwable $previous = null)
     {
-        $message = "UndefinedMagicMethodException: 📦 " . get_class($object) . " -> ⚡ ##" . $calledMethod ."##()";
+        $message = "UndefinedMagicMethodException: 📦 " . get_class($object) . " ⚡ ##" . $calledMethod ."##()";
         parent::__construct($message, $code, $previous);
     }
 }
