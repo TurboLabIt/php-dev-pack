a485c1e55856231b7689b1924f42d63d396283fc
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-09-04 15:19:13 +0200

    Update UndefinedMagicMethodException.php

--- src/Exception/UndefinedMagicMethodException.php
+++ src/Exception/UndefinedMagicMethodException.php
@@ -4,5 +4,9 @@ namespace TurboLabIt\TLIBaseBundle\Exception;
 
 class UndefinedMagicMethodException extends \Exception
 {
-
+    public function __construct(string $calledMethod, $code = 0, Throwable $previous = null)
+    {
+        $message = "UndefinedMagicMethodException: ##" . $calledMethod ."##";
+        parent::__construct($message, $code, $previous);
+    }
 }
